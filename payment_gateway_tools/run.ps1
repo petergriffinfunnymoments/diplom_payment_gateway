a2cb7f4df07b4dedf8397c9bbd5b7be25efca40c
petergriffinfunnymoments <petergriffinemailmoment@gmail.com>
2026-05-03 01:40:30 +0300

    Обновленная инструкция

--- payment_gateway_tools/run.ps1
+++ payment_gateway_tools/run.ps1
@@ -4,6 +4,4 @@ $env:YOOKASSA_SHOP_ID="1348152"
 $env:YOOKASSA_SECRET_KEY="test_-piTcwmu-KCZlaEmMNFXcXD5KaYrtObHLQFGmL2GFtM"
 
 $env:CARD_PAYMENT_PROVIDER="yookassa"
-$env:PAYMENT_RETURN_URL="https://ТВОЙ_LOCALTUNNEL_URL.loca.lt"
-
-go run ./cmd/payment-gateway
+$env:PAYMENT_RETURN_URL=""
